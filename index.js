ebd1c7d83e4cb0186ea0f94b8975cda7f9a81c8f
Hunter Kreshock <hkreshock@gmail.com>
2019-01-17 14:20:01 -0700

    attempt

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+'use strict';
+/* global $ */
 
-
-// grab user inputted text on add or "return"
-
-//check or uncheck items on the list by using "check button"
-
-//remove items permanently from list by hittin "delete"
+const shopping_list = [];
+function addNew(userTextElement) {
+  shopping_list.push(
+    newFunction());
+  
+  function newFunction() {
+    return ('<li><span class="shopping-item">' + (userTextElement) + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+  }
+}
+$(function () {
+  $('#shopping-list-entry').submit(event => {
+    event.preventDefault();
+    const userTextElement = $(event.currentTarget).find('#shopping-list-entry');
+    $('.shopping-item').text($(userTextElement).val());
+    return addNew();
+  });
+});
