7dcd0ad027b87572bcaaa0e992436b6d0a426ba0
JuiceboxJones <jderhalli@gmail.com>
2019-01-17 13:18:05 -0800

    able to add items to list

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+'use strict';
+/* global $*/
 
+// Get our general clone of item;
+let standardItem = entry => {
+  $('.shopping-list').append(` 
+    <li>
+      <span class="shopping-item">${entry}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+          </button>
+            <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`);
+};
 
 // grab user inputted text on add or "return"
-
+function getInput(){
+  return $('#shopping-list-entry').val();
+}
 //check or uncheck items on the list by using "check button"
 
 //remove items permanently from list by hittin "delete"
+
+function main() {
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    const userInput = getInput();
+    standardItem(userInput);
+  });
+}
+
+$(main);
